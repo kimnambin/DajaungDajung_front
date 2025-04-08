68235a148f8fd36c17d973d761deaa90434d3888
crossbat <xowls1999@gmail.com>
2025-04-08 12:30:49 +0900

    chore: header 완성 react-router-dom Link로 네비게이션 별 경로 설정 아직 app.jsx에서 router를 파주지는 않았음.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import Logo from '../assets/Logo.png'
 import searchIcon from '../assets/searchIcon.png'
 import { useState } from 'react'
 import './header.css'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Header() {
   let [login, setLogin] = useState('로그인');
@@ -15,9 +15,9 @@ function Header() {
           <img className='searchIcon' src={searchIcon} />
         </form>
         <div className='nav-items'>
-          <p className='chating'>채팅하기</p>
-          <p className='selling'>판매하기</p>
-          <p className='login'>{login}</p>
+          <Link to='/' className='chating'>채팅하기</Link>
+          <Link to='/items' className='selling'>판매하기</Link>
+          <Link to='/login' className='login'>{login}</Link>
         </div>
       </div>
     </nav>
