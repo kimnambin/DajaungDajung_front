c32a44446a793a9c9cca5733ea2f3b6e17b6957c
mingyeong0210 <mica7150@naver.com>
2025-05-07 18:02:04 +0900

    feat: 헤더 검색창

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,19 +2,32 @@ import Logo from '../../assets/Logo.png'
 import searchIcon from '../../assets/searchIcon.png'
 import { useState } from 'react'
 import styles from './header.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Header() {
   let [login, setLogin] = useState('로그인');
+  const [searchInput, setSearchInput] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if(searchInput.trim === '') return;
+    navigate(`/items?q=${encodeURIComponent(searchInput.trim())}`);
+  };
+
   return (
     <nav>
       <div className={styles.container}>
-        <Link to='/' className={styles.logo}>
+        <Link to='/dajungdajung' className={styles.logo}>
           <img src={Logo} className={styles.logo} />
         </Link>
-        <form className={styles.searchBox} method='post'>
-          <input placeholder='검색어를 입력하세요' />
-          <img className={styles.searchIcon} src={searchIcon} />
+        <form className={styles.searchBox} onSubmit={handleSearch}>
+          <input 
+            placeholder='검색어를 입력하세요'
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <img className={styles.searchIcon} src={searchIcon} alt='검색' onClick={handleSearch}/>
         </form>
         <div className={styles.navItems}>
           <Link to='/' className={styles.chating}>채팅하기</Link>
